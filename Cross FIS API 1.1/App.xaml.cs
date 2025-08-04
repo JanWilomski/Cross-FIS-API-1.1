42f3bbd4e75d8babd92ca0d62a940afd60be733c
janwil <jwilomski1@gmail.com>
2025-08-04 15:11:17 +0200

    Initial commit

---
+++ Cross FIS API 1.1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Cross_FIS_API_1._1;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
