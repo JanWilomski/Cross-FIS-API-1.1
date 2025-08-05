ba9cf9a85eca400c1579d11a92ed9fc80660e4b7
janwil <jwilomski1@gmail.com>
2025-08-05 12:05:58 +0200

    df

---
+++ Cross FIS API 1.1/MainWindowViewModel.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Cross_FIS_API_1._1.Services;
+using Cross_FIS_API_1._1.Commands;
+using Cross_FIS_API_1._1.Models;
+
+namespace Cross_FIS_API_1._1
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private readonly FisApiService _fisApiService;
+        private string _ipAddress = "172.31.136.4";
+        private string _port = "19593";
+        private string _user = "103";
+        private string _password = "glglgl";
+        private string _node = "24300";
+        private string _subnode = "14300";
+        private string _logText = "";
+        private string _statusText = "Ready";
+        private string _connectionStatus = "Disconnected";
+        private bool _isConnected = false;
+        private string _orderSymbol = "FTE";
+        private string _orderSide = "0";
+        private string _orderQuantity = "100";
+
+        public MainWindowViewModel()
+        {
+            _fisApiService = new FisApiService();
+            _fisApiService.MessageReceived += OnMessageReceived;
+            _fisApiService.ConnectionStatusChanged += OnConnectionStatusChanged;
+            _fisApiService.LogMessage += OnLogMessage;
+
+            ConnectCommand = new RelayCommand(async () => await ConnectAsync(), () => !string.IsNullOrEmpty(IpAddress));
+            SendOrderCommand = new RelayCommand(async () => await SendOrderAsync(), () => IsConnected);
+        }
+
+        #region Properties
+
+        public string IpAddress
+        {
+            get => _ipAddress;
+            set
+            {
+                _ipAddress = value;
+                OnPropertyChanged();
+                ((RelayCommand)ConnectCommand).RaiseCanExecuteChanged();
+            }
+        }
+
+        public string Port
+        {
+            get => _port;
+            set
+            {
+                _port = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string User
+        {
+            get => _user;
+            set
+            {
+                _user = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Node
+        {
+            get => _node;
+            set
+            {
+                _node = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Subnode
+        {
+            get => _subnode;
+            set
+            {
+                _subnode = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LogText
+        {
+            get => _logText;
+            set
+            {
+                _logText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string StatusText
+        {
+            get => _statusText;
+            set
+            {
+                _statusText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ConnectionStatus
+        {
+            get => _connectionStatus;
+            set
+            {
+                _connectionStatus = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsConnected
+        {
+            get => _isConnected;
+            set
+            {
+                _isConnected = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ConnectButtonText));
+                ((RelayCommand)SendOrderCommand).RaiseCanExecuteChanged();
+            }
+        }
+
+        public string ConnectButtonText => IsConnected ? "Disconnect" : "Connect";
+
+        public string OrderSymbol
+        {
+            get => _orderSymbol;
+            set
+            {
+                _orderSymbol = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string OrderSide
+        {
+            get => _orderSide;
+            set
+            {
+                _orderSide = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string OrderQuantity
+        {
+            get => _orderQuantity;
+            set
+            {
+                _orderQuantity = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand ConnectCommand { get; }
+        public ICommand SendOrderCommand { get; }
+
+        #endregion
+
+        #region Methods
+
+        private async Task ConnectAsync()
+        {
+            try
+            {
+                if (IsConnected)
+                {
+                    await _fisApiService.DisconnectAsync();
+                }
+                else
+                {
+                    StatusText = "Connecting...";
+                    
+                    var connectionParams = new FisConnectionParameters
+                    {
+                        IpAddress = IpAddress,
+                        Port = int.Parse(Port),
+                        UserNumber = User,
+                        Password = Password,
+                        CalledLogicalId = Node,
+                        CallingLogicalId = Subnode
+                    };
+
+                    await _fisApiService.ConnectAsync(connectionParams);
+                }
+            }
+            catch (Exception ex)
+            {
+                AddLog($"Connection error: {ex.Message}");
+                StatusText = "Connection failed";
+            }
+        }
+
+        private async Task SendOrderAsync()
+        {
+            try
+            {
+                if (!IsConnected)
+                {
+                    AddLog("Not connected to server");
+                    return;
+                }
+
+                StatusText = "Sending order...";
+                
+                var order = new OrderRequest
+                {
+                    Symbol = OrderSymbol,
+                    Side = OrderSide,
+                    Quantity = OrderQuantity,
+                    OrderType = "L", // Limit order
+                    Price = "100.00", // Default price
+                    Validity = "J" // Day order
+                };
+
+                await _fisApiService.SendOrderAsync(order);
+                AddLog($"Order sent: {OrderSymbol} {OrderSide} {OrderQuantity}");
+                StatusText = "Order sent";
+            }
+            catch (Exception ex)
+            {
+                AddLog($"Order error: {ex.Message}");
+                StatusText = "Order failed";
+            }
+        }
+
+        private void OnMessageReceived(object sender, FisMessageEventArgs e)
+        {
+            AddLog($"Received: {e.MessageType} - {e.Message}");
+        }
+
+        private void OnConnectionStatusChanged(object sender, ConnectionStatusEventArgs e)
+        {
+            IsConnected = e.IsConnected;
+            ConnectionStatus = e.IsConnected ? "Connected" : "Disconnected";
+            StatusText = e.StatusMessage;
+        }
+
+        private void OnLogMessage(object sender, LogMessageEventArgs e)
+        {
+            AddLog(e.Message);
+        }
+
+        private void AddLog(string message)
+        {
+            var timestamp = DateTime.Now.ToString("HH:mm:ss.fff");
+            LogText += $"[{timestamp}] {message}\n";
+            
+            // Keep log reasonable size
+            var lines = LogText.Split('\n');
+            if (lines.Length > 1000)
+            {
+                LogText = string.Join("\n", lines[^500..]);
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
---
+++ Cross FIS API 1.1/Services/MessageHelper.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Cross_FIS_API_1._1.Services
+{
+    public static class MessageHelper
+    {
+        /// <summary>
+        /// Konwertuje tablicę bajtów na reprezentację hex string dla celów debugowania
+        /// </summary>
+        /// <param name="bytes">Tablica bajtów do konwersji</param>
+        /// <param name="maxLength">Maksymalna długość do wyświetlenia</param>
+        /// <returns>Reprezentacja hex</returns>
+        public static string BytesToHexString(byte[] bytes, int maxLength = 50)
+        {
+            if (bytes == null || bytes.Length == 0)
+                return "";
+
+            int length = Math.Min(bytes.Length, maxLength);
+            var sb = new StringBuilder();
+            
+            for (int i = 0; i < length; i++)
+            {
+                sb.Append(bytes[i].ToString("X2"));
+                if (i < length - 1)
+                    sb.Append(" ");
+            }
+            
+            if (bytes.Length > maxLength)
+                sb.Append("...");
+                
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Waliduje podstawową strukturę wiadomości FIS
+        /// </summary>
+        /// <param name="message">Wiadomość do walidacji</param>
+        /// <returns>True jeśli struktura jest poprawna</returns>
+        public static bool ValidateFisMessage(byte[] message)
+        {
+            if (message == null || message.Length < 37) // LG(2) + Header(32) + Footer(3)
+                return false;
+
+            try
+            {
+                // Sprawdź długość wiadomości (pierwsze 2 bajty - LG)
+                int messageLength = message[0] + message[1] * 256;
+                if (messageLength != message.Length)
+                    return false;
+
+                // Sprawdź STX w nagłówku (powinien być na pozycji 2 i mieć wartość 2)
+                if (message[2] != 2) // STX
+                    return false;
+
+                // Sprawdź ETX w stopce (ostatni bajt powinien mieć wartość 3)
+                if (message[message.Length - 1] != 3) // ETX
+                    return false;
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Parsuje nagłówek wiadomości FIS
+        /// </summary>
+        /// <param name="message">Wiadomość</param>
+        /// <param name="headerOffset">Offset nagłówka (zwykle 2 - po LG)</param>
+        /// <returns>Sparsowany nagłówek lub null w przypadku błędu</returns>
+        public static FisHeader ParseHeader(byte[] message, int headerOffset)
+        {
+            if (message == null || message.Length < headerOffset + 32)
+                return null;
+
+            try
+            {
+                var header = new FisHeader();
+                int offset = headerOffset;
+
+                // STX (1 bajt)
+                header.STX = message[offset++];
+
+                // API Version (1 bajt)
+                header.ApiVersion = (char)message[offset++];
+
+                // Request Size (5 bajtów ASCII)
+                header.RequestSize = Encoding.ASCII.GetString(message, offset, 5);
+                offset += 5;
+
+                // Called Logical Identifier (5 bajtów ASCII)
+                header.CalledLogicalId = Encoding.ASCII.GetString(message, offset, 5);
+                offset += 5;
+
+                // Filler (5 bajtów)
+                offset += 5;
+
+                // Calling Logical Identifier (5 bajtów ASCII)
+                header.CallingLogicalId = Encoding.ASCII.GetString(message, offset, 5);
+                offset += 5;
+
+                // Filler (2 bajty)
+                offset += 2;
+
+                // Request Number (5 bajtów ASCII)
+                header.RequestNumber = Encoding.ASCII.GetString(message, offset, 5);
+                offset += 5;
+
+                // Pozostałe 3 bajty to filler
+                
+                return header;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Reprezentuje sparsowany nagłówek wiadomości FIS
+    /// </summary>
+    public class FisHeader
+    {
+        public byte STX { get; set; }
+        public char ApiVersion { get; set; }
+        public string RequestSize { get; set; } = string.Empty;
+        public string CalledLogicalId { get; set; } = string.Empty;
+        public string CallingLogicalId { get; set; } = string.Empty;
+        public string RequestNumber { get; set; } = string.Empty;
+    }
+}
